5846dc69e717a42f12b162ef29054f7431273bdf
guidopro <169035608+guidopro@users.noreply.github.com>
2025-04-09 15:54:47 +0100

    moves cv to public folder

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -2,7 +2,7 @@ export default function CV() {
   return (
     <>
       <embed
-        src="/src/assets/guymiddleton-cv.pdf"
+        src="./public/guymiddleton-cv.pdf"
         type="application/pdf"
         width="100%"
         height="1000px"
