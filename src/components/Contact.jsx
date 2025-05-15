dec18d2a460ecea6d4ba7b2c391c3004833f8f3e
guidopro <169035608+guidopro@users.noreply.github.com>
2025-05-15 17:37:41 +0100

    improves contact form styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,10 +8,7 @@ export default function Contact() {
       <div className="flex flex-col min-h-screen pt-15">
         <p className="flex flex-wrap justify-center text-center p-4 whitespace-pre text-white">
           Contact me using the form below or directly at{" "}
-          <a
-            href="mailto:guyvmiddletob@gmail.com"
-            className="hover:text-blue-500"
-          >
+          <a href="mailto:guyvmiddletob@gmail.com" className="underline">
             guyvmiddleton@gmail.com
           </a>
         </p>
@@ -20,13 +17,13 @@ export default function Contact() {
           <form
             method="post"
             name="contact_form"
-            className="flex flex-col m-5 p-6 gap-5 rounded-3xl shadow-2xl w-full max-w-xl bg-white"
+            className="flex flex-col m-5 p-6 gap-5 rounded-2xl shadow-2xl w-full max-w-xl bg-white"
             action="https://formsubmit.co/496ed51b4b21668146f84e005c99661c"
           >
             <div className="flex flex-col gap-1.5 p-2 m-1">
               <label htmlFor="fname">Your Name:</label>
               <input
-                className="border bg-cyan-50 rounded-sm"
+                className="border bg-cyan-50 rounded-md p-2"
                 type="text"
                 id="fname"
                 name="fname"
@@ -36,7 +33,7 @@ export default function Contact() {
             <div className="flex flex-col gap-1.5 p-2 m-1">
               <label htmlFor="email">Email Address:</label>
               <input
-                className="border bg-cyan-50 rounded-sm"
+                className="border bg-cyan-50 rounded-md p-2"
                 type="email"
                 id="email"
                 name="email"
@@ -48,7 +45,7 @@ export default function Contact() {
               <label htmlFor="message">Message:</label>
 
               <textarea
-                className="border bg-cyan-50 rounded-sm"
+                className="border bg-cyan-50 rounded-md h-37 p-2"
                 type="text"
                 id="message"
                 name="message"
